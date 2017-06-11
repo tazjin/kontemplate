5144842e97f67b21cb106dc45383f9691a6b69c5
Vincent Ambo <tazjin@gmail.com>
2017-06-11 23:44:27 +0200

    chore: Update Brew formula & Dockerfile to 1.1.0

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/tazjin/kontemplate"
-  url "https://github.com/tazjin/kontemplate/releases/download/v1.0.2/kontemplate-1.0.2-f79b261-darwin-amd64.tar.gz"
-  sha256 "5a2db5467bc77e4379b5b98f35c9864010f7023ae01a25fb5cda1aede59e021c"
-  version "1.0.2-f79b261"
+  url "https://github.com/tazjin/kontemplate/releases/download/v1.1.0/kontemplate-1.1.0-f7ce04e-darwin-amd64.tar.gz"
+  sha256 "1dccc80804589f6bd233dd79f52527f2ba8c4fa8d38857c80b2f47b504fe6c04"
+  version "1.1.0-f7ce04e"
 
   def install
     bin.install "kontemplate"
