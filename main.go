7bbc3cc033b8082f7758d4c2713846c8a8aadf59
Vincent Ambo <tazjin@gmail.com>
2017-11-04 12:36:45 +0100

    Version 1.3.0
    This release comes with minor usability improvements and features.
    
    * A new 'lookupIPAddr' template function is available for resolving
      DNS A records in templates. Thanks to @landro for the pull request!
    
    * Handling of "non-standard" resource set structures has been improved
      to result in better error messages and behaviour in several places.
    
    Release binaries are signed with GPG key `66F505681DB8F43B` which is
    verified on my Github profile.
    
    --------------
    
    Note: This is the last Kontemplate release that will be written in Go.
    
    Rob Pike's art project has proven its point but I believe it is
    ethically questionable and morally indefensible to continue on this
    path.
    
    You can track #72 for the Rust-rewrite of Kontemplate.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-const version string = "1.2.0"
+const version string = "1.3.0"
 
 // This variable will be initialised by the Go linker during the builder
 var gitHash string
