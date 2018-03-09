4b1d44f71bf243a86b90d047ef65d5b418b4f5bd
Vincent Ambo <tazjin@gmail.com>
2018-03-09 11:32:17 +0100

    refactor(build): Add Nix derivation & configure Travis to build it
    Adds a Nix-derivation for building kontemplate with dependencies
    pinned in Nix.

---
+++ default.nix
@@ -0,0 +1,33 @@
+# Copyright (C) 2016-2018  Vincent Ambo <mail@tazj.in>
+#
+# This file is part of Kontemplate.
+#
+# Kontemplate is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This file is the Nix derivation used to install Kontemplate on
+# Nix-based systems.
+
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs; buildGoPackage rec {
+  name = "kontemplate-${version}";
+  version = "master";
+  src = ./.;
+  goPackagePath = "github.com/tazjin/kontemplate";
+  goDeps = ./deps.nix;
+
+  # Enable checks and configure check-phase to include vet:
+  doCheck = true;
+  preCheck = ''
+    getGoDirs "" | parallel -j $NIX_BUILD_CORES buildGoDir vet
+  '';
+
+  meta = with lib; {
+    description = "A resource templating helper for Kubernetes";
+    homepage = "http://kontemplate.works/";
+    license = licenses.gpl3;
+  };
+}
