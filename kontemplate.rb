867f40307e9038406625bccf8047f627dd7cb148
Vincent Ambo <tazjin@gmail.com>
2018-03-17 23:16:45 +0100

    chore(brew): Bump Homebrew formula to 1.4.0

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/tazjin/kontemplate"
-  url "https://github.com/tazjin/kontemplate/releases/download/v1.3.0/kontemplate-1.3.0-98daa6b-darwin-amd64.tar.gz"
-  sha256 "4372e2c0f1249aa43f67e664335c20748d0b394ada1fcceefa3ae786e839ee47"
-  version "1.3.0-98daa6b"
+  url "https://github.com/tazjin/kontemplate/releases/download/v1.4.0/kontemplate-1.4.0-1f373ca-darwin-amd64.tar.gz"
+  sha256 "b034cfec6019c973ea7dd30297e1c3434c595e29106e557698ca24f3a4659e5a"
+  version "1.4.0-1f373ca"
 
   def install
     bin.install "kontemplate"
