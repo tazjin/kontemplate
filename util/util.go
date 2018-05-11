410145b5d829aafa25267f7d4bd2e603bf3bc700
Joshua Gardner <joshua.gardner@sendoutcards.com>
2018-05-11 14:03:49 -0600

    Add ability to provide extra variables through stdin

--- util/util.go
+++ util/util.go
@@ -10,10 +10,7 @@
 package util
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"strings"
 
 	"github.com/ghodss/yaml"
 )
@@ -45,19 +42,13 @@ func Merge(in1 *map[string]interface{}, in2 *map[string]interface{}) *map[string
 }
 
 // Loads either a YAML or JSON file from the specified path and deserialises it into the provided interface.
+// Treats JSON as YAML because that works fine.
 func LoadJsonOrYaml(filename string, addr interface{}) error {
 	file, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return err
 	}
 
-	if strings.HasSuffix(filename, "json") {
-		err = json.Unmarshal(file, addr)
-	} else if strings.HasSuffix(filename, "yaml") || strings.HasSuffix(filename, "yml") {
-		err = yaml.Unmarshal(file, addr)
-	} else {
-		return fmt.Errorf("File format not supported. Must be JSON or YAML.")
-	}
-
-	return nil
+	// Since valid JSON *is* valid YAML, just use the YAML Unmarshaler
+	return yaml.Unmarshal(file, addr)
 }
