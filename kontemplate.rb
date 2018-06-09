e9992aec0b690f32ed71aeef2dd73ea15669a2e9
Vincent Ambo <tazjin@gmail.com>
2018-06-09 21:58:42 +0200

    chore(brew): Update Homebrew formula for 1.6.0

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/tazjin/kontemplate"
-  url "https://github.com/tazjin/kontemplate/releases/download/v1.5.0/kontemplate-1.5.0-c68518d-darwin-amd64.tar.gz"
-  sha256 "61cd9ad3e28f52260458b707fd3120afa53e0610213ddd0ab03f489439f6b8a9"
-  version "1.5.0-c68518d"
+  url "https://github.com/tazjin/kontemplate/releases/download/v1.6.0/kontemplate-1.6.0-97bef90-darwin-amd64.tar.gz"
+  sha256 "d21529153d369d2347477f981a855525695c0bc2912a50f05d3baf15c96f7c16"
+  version "1.6.0-97bef90"
 
   def install
     bin.install "kontemplate"
