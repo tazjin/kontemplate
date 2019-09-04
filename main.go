6f71aedbb7962cd28d48f0591ee0ce1973171c58
Vincent Ambo <tazjin@google.com>
2019-09-04 11:15:40 +0100

    chore: Update copyright headers to 2019

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2016-2017  Vincent Ambo <mail@tazj.in>
+// Copyright (C) 2016-2019  Vincent Ambo <mail@tazj.in>
 //
 // Kontemplate is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
