88accc58cbe624c1327f914c2596216bb3b76789
Vincent Ambo <tazjin@google.com>
2019-09-04 12:05:10 +0100

    chore: Update image & Homebrew formula for 1.8.0 release

--- kontemplate.rb
+++ kontemplate.rb
@@ -3,9 +3,9 @@
 class Kontemplate < Formula
   desc "Kontemplate - Extremely simple Kubernetes resource templates"
   homepage "https://github.com/tazjin/kontemplate"
-  url "https://github.com/tazjin/kontemplate/releases/download/v1.7.0/kontemplate-1.7.0-511ae92-darwin-amd64.tar.gz"
-  sha256 "44910488c0e0480e306cc7b1de564a4c0d39013130f2f6e89bcd9a7401ef6a9a"
-  version "kontemplate-1.7.0-511ae92"
+  url "https://github.com/tazjin/kontemplate/releases/download/v1.8.0/kontemplate-1.8.0-6c3b299-darwin-amd64.tar.gz"
+  sha256 "c541f39ef14f4822ff2f5472a9c1e57f73f0277b6b85bec8e9514640aac311a8"
+  version "kontemplate-1.8.0-6c3b299"
 
   def install
     bin.install "kontemplate"
